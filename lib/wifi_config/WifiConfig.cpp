e292a972800cc6ce45d5c1e43f818753e99f5a10
htilssu <tolashuu@gmail.com>
2025-02-22 00:31:19 +0700

    ✨ (gitignore): add .gitignore file to ignore unnecessary files ✨ (idea): add .idea folder to store project-specific IDE settings ✨ (vscode): add .vscode folder to store project-specific VS Code settings ✨ (lib): add wifi_config library to handle WiFi configuration ✨ (platformio.ini): add WiFi and WebServer dependencies ✨ (main.cpp): implement web server to control LED

---
+++ lib/wifi_config/WifiConfig.cpp
@@ -0,0 +1,21 @@
+﻿#include "WifiConfig.h"
+#include <WiFi.h>
+#include <WebServer.h>
+
+const char* ssid = "THTrueLove";
+const char* password = "12345678";
+
+void startWiFi() {
+    Serial.begin(115200);
+    delay(10);
+
+    Serial.println();
+    Serial.print("Setting up Wi-Fi access point: ");
+    Serial.println(ssid);
+
+    WiFi.softAP(ssid, password);
+
+    Serial.println("Wi-Fi access point set up.");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.softAPIP());
+}
---
+++ lib/wifi_config/WifiConfig.h
@@ -0,0 +1,10 @@
+﻿//
+// Created by tolas on 22/02/2025.
+//
+
+#ifndef WIFICONFIG_H
+#define WIFICONFIG_H
+
+void startWiFi();
+
+#endif //WIFICONFIG_H
